50baf0daf9cb6ef700915d9ff29efd04babec198
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2019-11-23 12:36:48 -0700

    Write a solution for 07-registers to get around compiler misoptimizations

--- src/07-registers/src/main.rs
+++ src/07-registers/src/main.rs
@@ -1,6 +1,8 @@
 #![no_main]
 #![no_std]
 
+use core::ptr;
+
 #[allow(unused_imports)]
 use aux7::{entry, iprint, iprintln};
 
@@ -8,22 +10,45 @@ use aux7::{entry, iprint, iprintln};
 fn main() -> ! {
     aux7::init();
 
+    // Here we use volatile operations instead of plain reads/writes (explantion below main):
     unsafe {
-        // A magic address!
+        // A magical address!
         const GPIOE_BSRR: u32 = 0x48001018;
 
         // Turn on the "North" LED (red)
-        *(GPIOE_BSRR as *mut u32) = 1 << 9;
+        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 9);
 
         // Turn on the "East" LED (green)
-        *(GPIOE_BSRR as *mut u32) = 1 << 11;
+        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 11);
 
         // Turn off the "North" LED
-        *(GPIOE_BSRR as *mut u32) = 1 << (9 + 16);
+        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << (9 + 16));
 
         // Turn off the "East" LED
-        *(GPIOE_BSRR as *mut u32) = 1 << (11 + 16);
+        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << (11 + 16));
     }
+
 
     loop {}
 }
+
+// Below was the original code... Although it does work, when running with --release flag
+// the LLVM backend ends up misoptimizing and doesn't know GPIOE_BSRR is a register.
+// Instead of doing 4 bitshift operations on it, it misoptimizes to one.
+
+// unsafe {
+//     // A magic address!
+//     const GPIOE_BSRR: u32 = 0x48001018;
+
+//     // Turn on the "North" LED (red)
+//     *(GPIOE_BSRR as *mut u32) = 1 << 9;
+
+//     // Turn on the "East" LED (green)
+//     *(GPIOE_BSRR as *mut u32) = 1 << 11;
+
+//     // Turn off the "North" LED
+//     *(GPIOE_BSRR as *mut u32) = 1 << (9 + 16);
+
+//     // Turn off the "East" LED
+//     *(GPIOE_BSRR as *mut u32) = 1 << (11 + 16);
+// }

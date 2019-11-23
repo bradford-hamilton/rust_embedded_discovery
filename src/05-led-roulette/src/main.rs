ca882995d7ea6508a858ee85d35af3ba462252c8
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2019-11-22 22:11:09 -0700

    Led roulette solution

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -7,13 +7,34 @@ use aux5::{entry, prelude::*, Delay, Leds};
 #[entry]
 fn main() -> ! {
     let (mut delay, mut leds): (Delay, Leds) = aux5::init();
-    let half_period = 500_u16;
 
     loop {
-        leds[0].on();
-        delay.delay_ms(half_period);
+        for i in 0..8 {
+            if i == 7 {
+                leds[0].on();
+            } else if i == 8 {
+                leds[1].on()
+            } else {
+                leds[i + 1].on();
+            }
 
-        leds[0].off();
-        delay.delay_ms(half_period);
+            delay.delay_ms(50_u8);
+            leds[i].off();
+            delay.delay_ms(50_u8);
+        }
     }
+
+    // Above is my challenge solution and below is the authors solution
+
+    // let ms = 50_u8;
+    // loop {
+    //     for curr in 0..8 {
+    //         let next = (curr + 1) % 8;
+
+    //         leds[next].on();
+    //         delay.delay_ms(ms);
+    //         leds[curr].off();
+    //         delay.delay_ms(ms);
+    //     }
+    // }
 }

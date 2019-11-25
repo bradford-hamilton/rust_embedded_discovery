1497fb6a4355ed5f8220a49352a3db322dba86b1
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2019-11-24 20:16:58 -0700

    Set up main.rs for next challenge

--- src/11-usart/src/main.rs
+++ src/11-usart/src/main.rs
@@ -2,15 +2,26 @@
 #![no_main]
 #![no_std]
 
+use core::fmt::{self, Write};
+
 #[allow(unused_imports)]
-use aux11::{entry, iprint, iprintln};
+use aux11::{entry, iprint, iprintln, usart1};
+use heapless::{consts, Vec};
 
 #[entry]
 fn main() -> ! {
     let (usart1, mono_timer, itm) = aux11::init();
 
-    // Send a single character
-    usart1.tdr.write(|w| w.tdr().bits(u16::from(b'X')));
+    // A buffer with 32 bytes of capacity
+    let mut buffer: Vec<u8, consts::U32> = Vec::new();
 
-    loop {}
+    loop {
+        buffer.clear();
+
+        // TODO Receive a user request. Each user request ends with ENTER
+        // NOTE `buffer.push` returns a `Result`. Handle the error by responding
+        // with an error message.
+
+        // TODO Send back the reversed string
+    }
 }

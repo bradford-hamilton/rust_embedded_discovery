193b5c8bb5d2af033017775b10a2611b03850486
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2019-11-25 19:43:21 -0700

    Write his solution with comments for reference

--- src/11-usart/src/main.rs
+++ src/11-usart/src/main.rs
@@ -2,15 +2,14 @@
 #![no_main]
 #![no_std]
 
-use core::fmt::{self, Write};
-
 #[allow(unused_imports)]
 use aux11::{entry, iprint, iprintln, usart1};
 use heapless::{consts, Vec};
 
+// Maintainers solution
 #[entry]
 fn main() -> ! {
-    let (usart1, mono_timer, itm) = aux11::init();
+    let (usart1, _mono_timer, _itm) = aux11::init();
 
     // A buffer with 32 bytes of capacity
     let mut buffer: Vec<u8, consts::U32> = Vec::new();
@@ -18,10 +17,35 @@ fn main() -> ! {
     loop {
         buffer.clear();
 
-        // TODO Receive a user request. Each user request ends with ENTER
-        // NOTE `buffer.push` returns a `Result`. Handle the error by responding
-        // with an error message.
+        loop {
+            // Wait until there is data available
+            while usart1.isr.read().rxne().bit_is_clear() {}
 
-        // TODO Send back the reversed string
+            // Retrieve the data
+            let byte = usart1.rdr.read().rdr().bits() as u8;
+
+            // Push the byte into our buffer. When doing so, if
+            // the buffer is full -> return error message
+            if buffer.push(byte).is_err() {
+                // Buffer is full, write error back to usart
+                for byte in b"error: buffer full \n\r" {
+                    while usart1.isr.read().txe().bit_is_clear() {}
+                    usart1.tdr.write(|w| w.tdr().bits(u16::from(*byte)));
+                }
+
+                break;
+            }
+
+            // If the byte is a carriage return, iterate over our buffer
+            // in reverse and write it back to the usart
+            if byte == 13 {
+                for byte in buffer.iter().rev().chain(&[b'\n', b'\r']) {
+                    while usart1.isr.read().txe().bit_is_clear() {}
+                    usart1.tdr.write(|w| w.tdr().bits(u16::from(*byte)));
+                }
+
+                break;
+            }
+        }
     }
 }
